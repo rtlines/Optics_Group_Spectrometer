0ea125a63626461ca860b3ce7162fdad72b4d56b
opticsteam <optics.byui@gmail.com>
2025-04-04 17:29:25 -0600

    Organize the file system, took pictures of helium and krypton

--- camerawork.py
+++ camerawork.py
@@ -1,17 +1,22 @@
 from picamera2 import Picamera2, Preview
 from time import sleep
-from os import system
+from os import system, makedirs
 from datetime import datetime
 from numpy import empty, shape, arange
 from numpy import sum as npsum
 import matplotlib.pyplot as plt
 
-saveName=input("Name of spectrum: ")
+saveName=input("Gas Name (i.e. Neon): ")
+reason = input('Reason for taking this image: ')
 # Takes jpg and raw picture #############################################################################
+# names for all the files, folders for the files
 date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-filename = f'{date}_{saveName}_Picture.jpg'
+filename = f'{date}_Picture.jpg' # tool out _{saveName}
 #rawfilename = f'{date}_raw_file.raw'
 colfilename = f'{date}_{saveName}_Intensity_Graph.png'
+makedirs(saveName, exist_ok=True) # makes sure folder exists to store everything
+makedirs(f'{saveName}/color_photos', exist_ok=True) # checks for Gas/color_photos folder
+makedirs(f'{saveName}/spectra_plots', exist_ok=True) # checks for Gas/spectra_plots folder
 
 picam2 = Picamera2() 
 
@@ -22,7 +27,7 @@ picam2.start()
 sleep(2)
 
 # save to 2 file types: jpeg, raw
-metadata = picam2.capture_file(filename)
+metadata = picam2.capture_file(f'{saveName}/color_photos/{filename}') # saving the picture to the color_photos folder
 rawdata = picam2.capture_array('raw')
 
 # numpy array of the image (3 dimensions) ##############################################################
@@ -60,198 +65,31 @@ mid_pixels = pixels[start_row:end_row,:] # grabs just what's between start and e
 
 col_sum = npsum(mid_pixels, axis=0)
 
-# Show and Save
-# Plot image using pyplot, gray color map
-plt.imshow(pixels, cmap="gray")
-plt.show()
 
-# save cropped image
-plt.imsave(f'{date}_{saveName}_Cropped_Picture.jpg', mid_pixels, cmap = 'gray')
-
+'''
+# uncomment when aligning stuff
+plt.imsave(f'{date}_{saveName}_Cropped_Picture.jpg', mid_pixels, cmap = 'gray') # saving the cropped image
+'''
 # graphing the summed columns
 plt.plot(arange(len(col_sum)),col_sum,color='xkcd:russet')
 plt.title(f'{saveName} brightness across x-axis')
-plt.savefig(colfilename) # save the plot
-plt.show() # then show it
-
-print('done')
+plt.savefig(f'{saveName}/spectra_plots/{filename}') # saving the intensity graph
+# plt.show() # then show it
 
 #calibrate with color to find pos, find ang, etc. 
 
-'''
-##############################################################################
-
-# read_image rotate and process.py
-
-##############################################################################
-
-# This script reads in an image from a spectrometer and rotates it
-
-#   and someday will process it
-
-#
-
-##############################################################################
-
-# Author R. Todd Lines
-
-# Last Modified:  2024-11-06
-
-##############################################################################
-
-# Import libraries
-
-import numpy as np
-
-import matplotlib.pyplot as plt
-
-from PIL import Image
-
- 
-
- 
-
-# Read and process image. 
-
-image =Image.open("./solar_spectrum.jpg")
-
- 
-
-#The image is crooked, so streighten it
-
-image = image.rotate(-2.09 )
-
- 
-
- 
-
-# try to convert the image to an array
-
-im_array=np.array(image)
-
-# We are only going to deal with the images so average over the three colors
-
-#  This effectively makes a greyscale image
-
-grey_array = im_array[:, :, :3].mean(axis=2)  # Convert to grayscale
-
- 
-
- 
-
-# find the size of the array
-
-arr_side_length=np.shape(im_array)
-
-# short vertice dimension first, longer horizontal dimension second
-
-y_max=arr_side_length[0]
-
-x_max=arr_side_length[1]
-
-print("image shape in pixels: ", arr_side_length[0], "columns",arr_side_length[1],"rows",arr_side_length[2], "colors")
-
-#print(x_max,y_max)
-
- 
-
-# Pick a line to plot somewhere in the middle
-
-#line_to_plot=int(arr_side_length[0]/2)
-
- 
-
-# Make numbers for an x-axis that just go from zero to the side length
-
-x=np.arange(0,arr_side_length[1],1)
-
-y=np.arange(0,arr_side_length[0],1)
-
- 
-
-# Now split out one line of the image, say, the 500th row
-
-#y=grey_array[line_to_plot]
-
- 
-
-#Find the limits of the data
-
-y_ave=np.zeros(y_max)
-
-reduced_data=np.empty([1,x_max])
-
-j=0
-
-for i in range(y_max):
-
-    # np array indexing is [row, column]
-
-    y_ave[i]=np.average(grey_array[i,:])
-
-    if y_ave[i]>5:
-
-        j=j+1
-
-        reduced_data  = np.r_[reduced_data,[ grey_array[i,:]]]
-
-        #reduced_data = np.append(reduced_data, grey_array[i,:], axis=0)
-
- 
-
-print("number of rows with data", j)
-
- 
-
-   
-
-# Since we believe we streightened our image, average the columns so we get
-
-#  one line of data that shows our image.
-
-spec_ave=grey_array[:, :].mean(axis=0) 
-
- 
-
- 
-
-# Plot the image
-
-plt.subplot(221)
-
-plt.imshow(image)
-
-plt.axis("off")
-
- 
-
-#plot that line of data
-
-plt.subplot(222)
-
-plt.plot(x,spec_ave,linewidth=0.2)
-
-plt.axis("off")
-
- 
-
-#plot that which rows have data
-
-plt.subplot(223)
-
-plt.plot(y,y_ave,linewidth=0.2)
-
-plt.axis("off")
-
- 
-
-# Plot the  reduced image
-
-plt.subplot(224)
-
-plt.imshow(reduced_data)
-
-plt.axis("off")
-
+# Write information to log folder #################################################
+logfile = open(f'{saveName}/Log', 'a')
+logfile.write(f'{date}\n{reason}\n\n')
+logfile.close()
+
+# Show croppedh
+# Plot cropped image using pyplot, gray color map
+plt.imshow(mid_pixels, cmap="gray")
+plt.title('Cropped image - the values that will get summed')
 plt.show()
-'''
+
+
+print('done')
+
+
